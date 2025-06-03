2a499402445ff0c17f810a153e7ad673b8a4c649
Felix Rodriguez <felixalejandr33@gmail.com>
2025-06-03 11:19:43 +0200

    Clean imports

--- src/main/java/com/Api/Prices/Domain/Port/PricesRepository.java
+++ src/main/java/com/Api/Prices/Domain/Port/PricesRepository.java
@@ -1,8 +1,6 @@
 package com.Api.Prices.Domain.Port;
 
 import com.Api.Prices.Domain.Model.Prices;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
